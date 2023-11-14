d010d451a9906f9a43014d9aba25d36dbe4db571
Zrmikstri <tpvinhhcm@gmail.com>
2023-11-14 21:51:43 +0700

    using a torch.optim lr scheduler

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os
-from trainer import Trainer
 import numpy as np
 import torch
 import random
+from data_loaders import get_data_loader
+from trainer import Trainer
+from trainer import Trainer
 
 def set_random_seed(seed):
     if seed is None:
@@ -15,13 +16,52 @@ def set_random_seed(seed):
 
 def main():
     set_random_seed(42)
-    trainer = Trainer(dataset_name = 'CUB', architecture = 'resnet50', architecture_type = 'drop', pretrained = True,
-                 large_feature_map = True, drop_threshold = 0.8, drop_prob = 0.25, lr = 0.002, lr_classifier_ratio = 10.0,
-                 momentum = 0.9, weight_decay = 0.0001, lr_decay_points = [41, 61], lr_decay_rate = 0.2,
-                 sim_fg_thres = 0.4, sim_bg_thres = 0.2, loss_ratio_drop = 2.0,
-                 loss_ratio_sim = 0.5, loss_ratio_norm = 0.05, wsol_method = 'bridging-gap', 
-                 loader = voc_dataloader, log_dir = '/content/')
+    voc_dataloader = get_data_loader(data_roots='./voc', batch_size = 5, resize_size = 224)
 
+    trainer = Trainer(
+        dataset_name = 'VOC', 
+        architecture = 'resnet50', 
+        architecture_type = 'cam', 
+        pretrained = True,   
+        large_feature_map = True, 
+        drop_threshold = 0.8, 
+        drop_prob = 0.25, 
+        lr = 0.002, 
+        lr_classifier_ratio = 10.0,
+        momentum = 0.9, 
+        weight_decay = 0.0001, 
+        lr_decay_points = [21, 31], 
+        lr_decay_rate = 0.2,
+        sim_fg_thres = 0.4, 
+        sim_bg_thres = 0.2, 
+        loss_ratio_drop = 2.0,
+        loss_ratio_sim = 0.5, 
+        loss_ratio_norm = 0.05, 
+        wsol_method = 'cam', 
+        loader = voc_dataloader, 
+        log_dir = './result'
+    )
+
+    for epoch in range(40):
+        # Check warm epoch
+        warm = True if epoch < 10 else False
+
+        print(f'Epoch: {epoch + 1} {"(warm)" if warm else ""}')
+
+        result = trainer.train(warm=warm)
+        print(result)
+
+        if (epoch + 1) % 2 == 0:
+            result = trainer.evaluate()
+            print(f'Evaluate at epoch{epoch + 1}')
+            print(result)
+
+        print("---------------------------------\n")
+
+        if (epoch + 1) % 5 == 0:
+            trainer.save_checkpoint(epoch + 1)
+        
+        trainer.scheduler.step()
 
 if __name__ == '__main__':
     main()
