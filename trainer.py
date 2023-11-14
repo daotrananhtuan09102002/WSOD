7dbe9f67168356d2337a4edea275a2039a38d768
daotrananhtuan09102002 <79463278+daotrananhtuan09102002@users.noreply.github.com>
2023-11-14 22:00:38 +0700

    fix bug

--- trainer.py
+++ trainer.py
@@ -59,7 +59,7 @@ class Trainer(object):
             self.criterion = nn.BCEWithLogitsLoss().cuda()
 
         if type_metric == 'mAP':
-            self.metrics = metrics.MultilabelAUROC(num_labels=self._NUM_CLASSES_MAPPING[self.dataset_name]).to('cuda')
+            self.metrics = metrics.MulticlassAUPRC(num_labels=self._NUM_CLASSES_MAPPING[self.dataset_name]).to('cuda')
         elif type_metric == 'acc':
             self.metrics = metrics.MultilabelAccuracy().to('cuda')
         self.l1_loss = nn.L1Loss().cuda()
