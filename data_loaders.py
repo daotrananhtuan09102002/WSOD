90e457d2295facbd7df9acd915e6f5120992b4fd
daotrananhtuan09102002 <79463278+daotrananhtuan09102002@users.noreply.github.com>
2023-12-03 15:14:09 +0700

    fix bug

--- data_loaders.py
+++ data_loaders.py
@@ -157,6 +157,7 @@ def get_data_loader(data_roots, batch_size, resize_size, augment=False):
         dataset_transforms = dict(
             train=transforms.Compose([
                 transforms.Resize((resize_size, resize_size)),
+                transforms.AutoAugment(),
                 transforms.ToImageTensor(),
                 transforms.ConvertImageDtype(torch.float),
                 transforms.Normalize(_IMAGE_MEAN_VALUE, _IMAGE_STD_VALUE)
